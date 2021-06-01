1d317aa2e765443c01285c0cb6ee88c658a28c80
Maxi Aragon <jose.maximiliano.aragon@gmail.com>
2021-06-01 18:03:51 -0300

    test buildspec

--- dockerstack/docker/php7.4.19-apache-buster-prod/startup.sh
+++ dockerstack/docker/php7.4.19-apache-buster-prod/startup.sh
@@ -2,5 +2,6 @@
 # Exec artisan commands before start php-fpm
 # Use www user
 #su www -c  "/usr/local/bin/php /var/www/artisan migrate --force"
-
+mkdir storage/database
+touch storage/database/service.sqlite
 apache2-foreground;

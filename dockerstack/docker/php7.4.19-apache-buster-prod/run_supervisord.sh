3189b102a42a7d3e77d11e3e32978780014c0a8f
Maxi Aragon <jose.maximiliano.aragon@gmail.com>
2021-06-09 22:23:26 -0300

    +x sqlite

--- dockerstack/docker/php7.4.19-apache-buster-prod/run_supervisord.sh
+++ dockerstack/docker/php7.4.19-apache-buster-prod/run_supervisord.sh
@@ -2,5 +2,6 @@
 mkdir /var/www/storage/
 mkdir /var/www/storage/database
 touch /var/www/storage/database/service.sqlite
+chmod +x /var/www/storage/database/service.sqlite
 php artisan migrate:fresh --force
 /usr/bin/supervisord -n -c /etc/supervisord.conf

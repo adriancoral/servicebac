67506f253e0927db3efe4fe6938c75973d58680f
Maxi Aragon <jose.maximiliano.aragon@gmail.com>
2021-07-14 15:23:44 -0300

    cambio de cluster y cors

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -8,7 +8,7 @@ const app = new cdk.App();
 
 const infraStack = new Infra(app, 'services-stack', {
   containerImage: 'latest',
-  clusterInstanceType: 't2.micro',
+  clusterInstanceType: 't3a.nano',
   certificateArn: 'arn:aws:acm:us-east-1:352725560891:certificate/0f32f657-dc1e-4e6f-84ae-167d52c07b96',
   hostzone: {
     id: 'Z04944581I6URJMSLZPD2',
